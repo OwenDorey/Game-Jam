bc2882fb081bf204c75560f9dae34d9290a48ec5
OwenDorey <16odorey@gmail.com>
2023-11-16 17:01:45 +0000

    Tilemaps

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -5,9 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public Animator fade;
+    public float fadeTime = 1f;
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
         Cursor.visible = false;
+        StartCoroutine(Load(1));
+    }
+
+    IEnumerator Load(int levelIndex)
+    {
+        fade.SetTrigger("Fade");
+
+        yield return new WaitForSeconds(fadeTime);
+
+        SceneManager.LoadScene(levelIndex);
     }
 }
